2a38b128333155b9d8a8dd5d0f99e1efd618293c
Lex <lex@gouwestadmm.nl>
2013-06-12 10:39:52 +0200

    Initial

---
+++ _site/js/main.js
@@ -0,0 +1,93 @@
+/*    
+       _____  ____  _    ___          ________  _____ _______       _____  
+      / ____|/ __ \| |  | \ \        / /  ____|/ ____|__   __|/\   |  __ \ 
+     | |  __| |  | | |  | |\ \  /\  / /| |__  | (___    | |  /  \  | |  | |
+     | | |_ | |  | | |  | | \ \/  \/ / |  __|  \___ \   | | / /\ \ | |  | |
+     | |__| | |__| | |__| |  \  /\  /  | |____ ____) |  | |/ ____ \| |__| |
+      \_____|\____/ \____/    \/  \/   |______|_____/   |_/_/    \_\_____/ 
+                                                                  
+          |\/|  _. ._ |   _ _|_ o ._   _    ()    |\/|  _   _| o  _. 
+          |  | (_| |  |< (/_ |_ | | | (_|   (_X   |  | (/_ (_| | (_| 
+                                       _|                            
+                             
+                              © www.gouwestad.com
+                         
+                           WHY ARE YOU LOOKING HERE? ;)
+*/
+
+
+
+
+
+
+/****************************************************************************
+
+	SOME SMOOTH SCROLLING ANCHOR LINKS | COURTESY OF CSS-TRICKS
+
+*****************************************************************************/
+
+$(document).ready(function() {
+  function filterPath(string) {
+  return string
+    .replace(/^\//,'')
+    .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
+    .replace(/\/$/,'');
+  }
+  var locationPath = filterPath(location.pathname);
+  var scrollElem = scrollableElement('html', 'body');
+ 
+  $('a[href*=#]').each(function() {
+    var thisPath = filterPath(this.pathname) || locationPath;
+    if (  locationPath == thisPath
+    && (location.hostname == this.hostname || !this.hostname)
+    && this.hash.replace(/#/,'') ) {
+      var $target = $(this.hash), target = this.hash;
+      if (target) {
+        var targetOffset = $target.offset().top;
+        $(this).click(function(event) {
+          event.preventDefault();
+          $(scrollElem).animate({scrollTop: targetOffset}, 400, function() {
+            location.hash = target;
+          });
+        });
+      }
+    }
+  });
+ 
+  // use the first element that is "scrollable"
+  function scrollableElement(els) {
+    for (var i = 0, argLength = arguments.length; i <argLength; i++) {
+      var el = arguments[i],
+          $scrollElement = $(el);
+      if ($scrollElement.scrollTop()> 0) {
+        return el;
+      } else {
+        $scrollElement.scrollTop(1);
+        var isScrollable = $scrollElement.scrollTop()> 0;
+        $scrollElement.scrollTop(0);
+        if (isScrollable) {
+          return el;
+        }
+      }
+    }
+    return [];
+  }
+ 
+});
+
+
+
+// Show the sliding nav
+$("#nav-btn").click(function (e) {
+   e.preventDefault();
+  $("#slide-nav").toggleClass("visible");
+  $(".body-overlay").toggleClass("visible");
+});
+
+$("#nav-btn-close").click(function (e) {
+   e.preventDefault();
+  $("#slide-nav").toggleClass("visible");
+  $(".body-overlay").toggleClass("visible");
+});
+
+
